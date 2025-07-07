09716a82a41c61eb4553ec191c5149e5a0c8ef76
Roy Ren <jiahuaren135@gmail.com>
2025-07-06 18:53:05 -0700

    potentially fix not found error on github page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <BrowserRouter basename="/portfolio/">
         <Routes>
           <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/portfolio/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
