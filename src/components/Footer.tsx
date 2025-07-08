0acdba44c447c2121359cf58624c3068c061467a
Roy Ren <jiahuaren135@gmail.com>
2025-07-07 22:34:07 -0700

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,24 @@
-import { ArrowUp } from "lucide-react";
+import { ArrowUp, Github } from "lucide-react";
 
 export const Footer = () => {
   const currentYear: number = new Date().getFullYear();
   return (
-    <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
-      <p className="text-sm text-muted-foreground ">
-        &copy; {currentYear} Jiahua Ren
-      </p>
+    <footer className="py-8 px-4 bg-card relative border-t border-border mt-12 pt-8 flex justify-between items-start">
+      <div className="flex flex-col space-y-1">
+        <p className="text-sm text-muted-foreground ">
+          &copy; {currentYear} Jiahua Ren
+        </p>
+        <p className="text-sm text-muted-foreground flex items-center pl-12">
+          Github repo of this site:{" "}
+          <a
+            href="https://github.com/jia-hua-ren/portfolio"
+            target="_blank"
+            className="ml-1 hover:text-primary transition-colors"
+          >
+            <Github size={16} />
+          </a>
+        </p>
+      </div>
 
       <a
         href="#me"
