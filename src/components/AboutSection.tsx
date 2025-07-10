8d2e7d652169a0c0f0ac0ec1c3c11f06468dde16
Roy Ren <jiahuaren135@gmail.com>
2025-07-10 10:48:27 -0700

    edit about me section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,4 +1,4 @@
-import { Code, User, Briefcase } from "lucide-react";
+// import { Code, User, Briefcase } from "lucide-react";
 
 export const AboutSection = () => {
   return (
@@ -8,9 +8,10 @@ export const AboutSection = () => {
           About <span className="text-primary"> Me</span>
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+        {/* md:grid-cols-2 */}
+        <div className="grid grid-cols-1 gap-12 items-center">
           <div className="space-y-6">
-            <h3 className="text-2xl font-semibold">Passionate CS student</h3>
+            {/* <h3 className="text-2xl font-semibold">Passionate CS student</h3> */}
 
             <p className="text-muted-foreground">
               I'm a passionate Computer Science student with a curiosity that
@@ -37,17 +38,17 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
 
-              <a
+              {/* <a
                 href="#cv"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors durantion-300"
               >
                 {" "}
                 Download CV
-              </a>
+              </a> */}
             </div>
           </div>
 
-          <div className="grid grid-cols-1 gap-6">
+          {/* <div className="grid grid-cols-1 gap-6">
             <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
                 <div className="p-3 rounded-full bg-primary-10">
@@ -88,7 +89,7 @@ export const AboutSection = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
