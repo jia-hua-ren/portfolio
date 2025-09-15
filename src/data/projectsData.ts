3accb2a52afd4d03e7a04798371818ac9ccb700e
Roy Ren <jiahuaren135@gmail.com>
2025-09-14 20:40:55 -0700

    add new projects, add pictures

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -9,15 +9,6 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    title: "Student Information System",
-    description:
-      "Designed and implemented relational database schemas with various integrity constraints for a fictional college's student and course information, which can be accessed through a Java CLI",
-    image: "/portfolio/projects/studentInfoSystem.png",
-    tags: ["SQL", "Java", "Oracle JDBC", "Database"],
-    url: "#",
-    githubUrl: "https://github.com/jia-hua-ren/dbproj174a",
-  },
-  {
     title: "Code&Grow",
     description:
       "A website for helping users learn programming languages through online lessons, built-in code editor for practice, and AI feedback. Created with a team of 6 practicing Agile development.",
@@ -38,9 +29,36 @@ export const projects: Project[] = [
     description:
       "A capstone project with 5 people, mentored by Aaron Buchwald from AvaLabs. A database system that is built upon AvalanchGo's interface, which utilizes the merkle tree's own structure to store on disk directly, bypassing the need for an often inefficient key-value store.",
     image: "/portfolio/projects/avalution.png",
-    tags: ["Go", "Database"],
+    tags: ["Go", "File Systems", "Database", "Blockchain"],
     url: "https://capstone.cs.ucsb.edu/teams.html#team10",
     githubUrl: "https://github.com/jlang61/avalution",
+  },
+  {
+    title: "Gamefied To-Do List",
+    description:
+      "Built with a Next.js frontend and a Spring Boot backend, deployed on Render.com with a Docker container. Utilizes a Postgres SQL database to store user data. This webapp aims to make finishing tasks fun by awarding exp points to users.",
+    image: "/portfolio/projects/todo.png",
+    tags: [
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Next.js",
+      "Spring Boot",
+      "Java",
+      "PostgreSQL",
+      "Docker",
+    ],
+    url: "https://todoleveling.onrender.com/",
+    githubUrl: "https://github.com/jia-hua-ren/todoleveling",
+  },
+  {
+    title: "Student Information System",
+    description:
+      "Designed and implemented relational database schemas with various integrity constraints for a fictional college's student and course information, which can be accessed through a Java CLI",
+    image: "/portfolio/projects/studentInfoSystem.png",
+    tags: ["SQL", "Java", "Oracle JDBC", "Database"],
+    url: "#",
+    githubUrl: "https://github.com/jia-hua-ren/dbproj174a",
   },
   {
     title: "Portfolio (this website!)",
@@ -55,48 +73,22 @@ export const projects: Project[] = [
     title: "Research on a database of Functionally Equivalent C code pairs",
     description:
       "Research project for my studying aborad period at Osaka University, I worked under Prof. Yoshiki Higo to research on a databse of functionally equivalent C code pairs, which can be used to train code clone detection models. Utilized Python, bash, Docker, and various tools and libraries to process C and C++ code and test pairs for functional equivalence. More details in the repo.",
-    image: "",
-    tags: ["C", "C++", "Python", "Docker", "bash", "GoogleTest"],
+    image: "/portfolio/projects/research.png",
+    tags: ["C", "C++", "Python", "Docker", "bash"],
     url: "#",
     githubUrl: "https://github.com/jia-hua-ren/2024SpringSummerResearch",
-  },
-  {
-    title: "UCSB CS156 Legacy Code Project",
-    description:
-      "A project for CS156 at UCSB, where I worked with a team in an Agile environment to improve and add new features to a legacy codebase. Practiced creating useful PR descriptions and code reviews for other PRs. I focused on mutation testing and improving the test coverage of code.",
-    image: "",
-    tags: [
-      "Java",
-      "JUnit",
-      "Spring Boot",
-      "Mutation Testing",
-      "Agile",
-      "Storybook",
-      "GitHub Actions",
-    ],
-    url: "https://courses.dokku-02.cs.ucsb.edu/",
-    githubUrl: "https://github.com/ucsb-cs156-s25/proj-courses-s25-02/pull/27",
   },
   {
     title: "Chinkara goat paradise (PyWeek 35 Game Jam)",
     description:
       "A game made with a team of 3 for PyWeek 35 Game Jam. We utilized the pygames library and created the game in seven days. I worked on the game mechanics, player and enemy logic.",
     image: "/portfolio/projects/chinkara.png",
-    tags: ["Python", "Pygame", "Game Development"],
+    tags: ["Python", "Game Development"],
     url: "https://pyweek.org/e/chinkaraoshiman/",
     githubUrl: "https://github.com/jia-hua-ren/pyweek-35-chinkaraoshiman",
   },
   {
-    title: "KOS",
-    description:
-      "Lab Project for CS170 at UCSB, implementing the kernle for an operating system for a simulated MIPS machine, which includes process management, memory management, pipes, basic system calls, etc.",
-    image: "",
-    tags: ["C"],
-    url: "https://sites.cs.ucsb.edu/~rich/class/cs170/labs.html",
-    githubUrl: "#",
-  },
-  {
-    title: "Chromatic Tunor",
+    title: "Chromatic Tuner",
     description:
       "An Embedded System project on a FPGA board in C that implements a chromatic tuner, which detects the frequency of the sound with FFT and displays the closest note.",
     image: "",
